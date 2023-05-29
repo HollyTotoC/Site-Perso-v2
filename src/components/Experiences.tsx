e4aaae5261115a3c515b67c0f8435904155a6661
Théo-toto Certa <totoc.pro@gmail.com>
2023-05-30 00:08:08 +0200

    experiences.tsx link to json

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -1,84 +1,39 @@
+import { useState } from "react";
+import experiences from "../assets/experiences.json";
+
 function Experiences() {
+    const [collapsedStates, setCollapsedStates] = useState(
+        experiences.map(() => true) // Initial state: all items are collapsed
+    );
+
+    const toggleCollapse = (index: number) => {
+        setCollapsedStates((prevStates) => {
+            const newStates = [...prevStates];
+            newStates[index] = !newStates[index]; // Toggle the state of the item at `index`
+            return newStates;
+        });
+    };
+
     return (
         <section id="experiences">
             <h2 className="title">Experiences</h2>
             <div className="container">
-                <article className="exp">
-                    <h3 className="exp_title">2023</h3>
-                    <p className="exp_text">
-                        Actuellement, je m'épanouis dans le monde du freelance,
-                        renforçant et élargissant mes compétences en
-                        développement web. En tant que développeur indépendant,
-                        j'applique les fondamentaux de la programmation web et
-                        de la conception de sites, acquises lors de ma
-                        formation, en créant des solutions personnalisées pour
-                        mes clients. Je code des sites web en HTML, CSS, et
-                        JavaScript, et j'utilise des frameworks tels que React
-                        et Node.js pour créer des applications web dynamiques.
-                        Cette expérience en freelance me permet de me confronter
-                        à de nouveaux défis, d'approfondir ma connaissance des
-                        technologies actuelles et d'en découvrir de nouvelles.
-                    </p>
-                </article>
-                <article className="exp">
-                    <h3 className="exp_title">2022</h3>
-                    <p className="exp_text">
-                        Formation en ligne de développement web couvrant les
-                        fondamentaux de la programmation web et de la conception
-                        de sites web. Au cours de la formation, j'ai appris à
-                        coder des sites web en HTML, CSS et JavaScript, ainsi
-                        qu'à utiliser des frameworks tels que React et Node.js
-                        pour créer des applications web dynamiques.
-                    </p>
-                </article>
-                <article className="exp">
-                    <h3 className="exp_title">2022</h3>
-                    <p className="exp_text">
-                        Alternance en entreprise de développement web où j'ai pu
-                        acquérir une expérience professionnelle en utilisant les
-                        compétences acquises lors de ma formation. J'ai
-                        travaillé sur des projets concrets, en collaboration
-                        avec d'autres développeurs, pour créer des applications
-                        web robustes et performantes.
-                    </p>
-                </article>
-                <article className="exp">
-                    <h3 className="exp_title">2020 - 2023</h3>
-                    <p className="exp_text">
-                        En tant que consultant en communication digitale, j'ai
-                        aidé mes clients à développer leur présence en ligne et
-                        à atteindre leur public cible. J'ai travaillé avec eux
-                        pour élaborer des stratégies de marketing numérique, des
-                        plans de contenu et des campagnes publicitaires. J'ai
-                        également supervisé la création de contenu web et vidéo
-                        pour m'assurer qu'il était cohérent avec leur marque et
-                        leurs objectifs.
-                    </p>
-                </article>
-                <article className="exp">
-                    <h3 className="exp_title">2013 - 2022</h3>
-                    <p className="exp_text">
-                        J'ai utilisé mes compétences en motion design et en
-                        montage pour aider mes clients à créer des vidéos qui
-                        communiquent efficacement leur message. J'ai travaillé
-                        sur une variété de projets, allant des publicités aux
-                        vidéos promotionnelles en passant par les animations 2D
-                        et 3D. J'ai géré le processus de création de la
-                        conception à la livraison finale.
-                    </p>
-                </article>
-                <article className="exp">
-                    <h3 className="exp_title">2010 - 2022</h3>
-                    <p className="exp_text">
-                        En tant que réalisateur et vidéaste indépendant, j'ai
-                        géré des projets de A à Z, de la prise de contact avec
-                        le client à la livraison finale. J'ai travaillé sur une
-                        variété de projets allant des vidéos d'entreprise aux
-                        courts métrages en passant par les documentaires. J'ai
-                        géré le tournage, le montage, la post-production et la
-                        diffusion des projets.
-                    </p>
-                </article>
+                {experiences.map((experience, index) => {
+                    return (
+                        <article className="exp" key={index}>
+                            <h3 className="exp_title">{experience.years}</h3>
+                            <p
+                                className="exp_text"
+                                onClick={() => toggleCollapse(index)}
+                            >
+                                {experience.part1}{" "}
+                                {collapsedStates[index] && "..."}
+                                {!collapsedStates[index] &&
+                                    `${experience.part2}`}
+                            </p>
+                        </article>
+                    );
+                })}
             </div>
         </section>
     );
