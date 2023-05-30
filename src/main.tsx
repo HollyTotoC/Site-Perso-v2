671a1ab2fae2667b642413316e0f6bf69bf8a4a3
Théo-toto Certa <totoc.pro@gmail.com>
2023-05-30 12:38:34 +0200

    google analytics et react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import "./assets/styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <Home />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
